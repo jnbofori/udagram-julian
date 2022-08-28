1b5cdb9d88d0b8e91a63c0cda8ad212e07cfa992
Julian <jofori@infonaligy.com>
2022-08-28 03:39:49 +0000

    implemented endpoint to filter images

--- src/server.ts
+++ src/server.ts
@@ -32,6 +32,23 @@ import * as path from 'path';
   /**************************************************************************** */
 
   //! END @TODO1
+
+  app.get('/filteredimage', async (req, res) => {
+    try {
+      let storedFiles = fs.readdirSync(`${__dirname}/util/tmp/`);
+      const files = storedFiles.map((file) => path.join(__dirname, "util", "tmp", file))
+      deleteLocalFiles(files);
+
+      let { image_url } = req.query;
+      if (!image_url) {
+        return res.status(400).send({ message: 'Image url is required' });
+      }
+      const filteredPath = await filterImageFromURL(image_url);
+      res.status(200).sendFile(filteredPath);
+    } catch(e) {
+      res.status(500).send("Error processing image");
+    }
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
--- src/util/util.ts
+++ src/util/util.ts
@@ -34,6 +34,8 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
-    fs.unlinkSync(file);
+    if (fs.existsSync(file)) {
+      fs.unlinkSync(file);
+    }
   }
 }
